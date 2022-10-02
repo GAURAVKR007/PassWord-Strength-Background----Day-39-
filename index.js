6c0c44e514f37b5ee63c6902ed5ce25e3b7f31c8
GAURAVKR007 <gauravkrthakur07@gmail.com>
2022-10-02 12:58:42 +0530

    Added the blur effect to the image in js [Project Completed]

---
+++ index.js
@@ -0,0 +1,11 @@
+const password = document.querySelector('#password')
+const background = document.getElementById('background')
+
+password.addEventListener('input',(e)=>{
+    const input = e.target.value
+    const length = input.length
+
+    const blurValue = (20 - length * 2)
+
+    background.style.filter = `blur(${blurValue}px)`
+})
